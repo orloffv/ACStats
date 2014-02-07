ad6e677a135878930d8ee1b01331e0480c40a2bc
orloffv <orloff.v@gmail.com>
2014-02-07 15:29:52 +0400

    feature(index action with all methods)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,19 @@
 (function () {
     "use strict";
+
+    var _ = require('underscore');
+
     module.exports = function(app, mongoose, log) {
         var routes = {
             index: function(req, res) {
-                res.send('see /api/events, /api/users, /api/servers, /api/hits');
+                var routes = [];
+                if (app.routes && app.routes.get) {
+                    routes = _.map(app.routes.get, function(route) {
+                        return route.path;
+                    });
+                }
+
+                res.send('see<br> ' + _.reduce(routes, function(memo, route) {return memo + route + '<br>';}));
             }
         };
 
