7cff00dff85b9cedc185a9faab27c6ecf0b6d7dd
orloffv <orloff.v@gmail.com>
2014-02-11 15:58:50 +0400

    fix(add allow headers)

--- app/main.js
+++ app/main.js
@@ -10,7 +10,8 @@
         var allowCrossDomain = function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Headers", "origin, content-type, accept, x-mime-type, x-requested-with, x-file-name, pragma, cache-control");
+
             next();
         };
 
